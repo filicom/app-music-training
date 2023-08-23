9c6974bbd9b4dda7c9b4274011e159cb1f46ec27
Felipe Guedes <felipebatista2019@hotmail.com>
2023-08-23 10:49:31 -0300

    FEAT: criação do arquivo percepcao.js

--- js/piano.js
+++ js/piano.js
@@ -1,27 +1,27 @@
-const teclas = document.querySelectorAll(".tecla");
+const teclas = document.querySelectorAll(".tecla");      //seleciona todas teclas
 teclas.forEach((tecla) => {
-    tecla.addEventListener("mousedown", () => clicoumousedown(tecla))
+    tecla.addEventListener("mousedown", () => clicoumousedown(tecla))        //evento para cada tecla após clicar
     tecla.addEventListener("mouseup", () => clicoumouseup(tecla))
 })
-const tocarNota = (nota) => {
-    const audio = new Audio(`../notas/${nota}.wav`)
-    audio.play()
-}
-
 const clicoumousedown = (tecla) => {
-    tocarNota(tecla.getAttribute('data-nota'))
+    tocarNota(tecla.getAttribute('data-nota'))     //altera estilo da tecla após clicar
 
     if (tecla.className.includes("preta")) {
         tecla.classList.add("preta-pressionada")
         return;
     }
     tecla.classList.add("branca-pressionada")
-
+    
 }
 const clicoumouseup = (tecla) => {
-    if (tecla.className.includes("preta")) {
+    if (tecla.className.includes("preta")) {            //altera estilo da tecla após clicar
         tecla.classList.remove("preta-pressionada")
         return;
     }
     tecla.classList.remove("branca-pressionada")
+}
+
+const tocarNota = (nota) => {                           //busca da nota no arquivo de audio
+    const audio = new Audio(`../notas/${nota}.wav`)
+    audio.play()
 }
