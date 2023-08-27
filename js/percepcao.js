78fdec1a1cba18b0f7d26a4e1756eedc65c26bb3
Felipe Guedes <felipebatista2019@hotmail.com>
2023-08-27 15:17:45 -0300

    FEAT: criado script para funcionamento do contador de acertos

--- js/percepcao.js
+++ js/percepcao.js
@@ -1,47 +1,54 @@
-const frm = document.querySelector("form")  //formulário com os botões
-const resp = document.querySelector("h3")  //saída para contador de acertos
-const teclas = document.querySelectorAll(".tecla");      //seleciona todas teclas
+const iniciar = document.getElementById("iniciar");
+const resp = document.querySelector("h3");
+const teclas = document.querySelectorAll(".tecla");
 
-
-teclas.forEach((tecla) => {
-  tecla.addEventListener("mousedown", () => clicoumousedown(tecla))        //evento para cada tecla após clicar
-  tecla.addEventListener("mouseup", () => clicoumouseup(tecla))
-})
+let contador = 0;
+let NotaAleatoria = "";
+let NotaTocada = "";
 
 const clicoumousedown = (tecla) => {
-  tocarNota(tecla.getAttribute('data-nota'))     //altera estilo da tecla após clicar
+  if (NotaAleatoria !== "") {
+    NotaTocada = tecla.getAttribute('data-nota');
+    
+    if (NotaTocada === NotaAleatoria) {
+      contador++;
+      resp.innerText = "Acertos: " + contador; 
+    }
+  }
+ 
+  tocarNota(tecla.getAttribute('data-nota'));
 
   if (tecla.className.includes("preta")) {
-      tecla.classList.add("preta-pressionada")
-      return;
+    tecla.classList.add("preta-pressionada");
+    return;
   }
-  tecla.classList.add("branca-pressionada")
-  
-}
+  tecla.classList.add("branca-pressionada");
+};
+
 const clicoumouseup = (tecla) => {
-  if (tecla.className.includes("preta")) {            //altera estilo da tecla após clicar
-      tecla.classList.remove("preta-pressionada")
-      return;
+  if (tecla.className.includes("preta")) {
+    tecla.classList.remove("preta-pressionada");
+    return;
   }
-  tecla.classList.remove("branca-pressionada")
-}
+  tecla.classList.remove("branca-pressionada");
+};
 
 const tocarNota = (nota) => {
-  const audio = new Audio(`../notas/${nota}.wav`)   //busca da nota no arquivo de audio
-  audio.play()
-}
+  const audio = new Audio(`../notas/${nota}.wav`);
+  audio.play();
+};
 
 const RandomNumber = () => {
-return  Math.floor(Math.random()*25) + 1     //gera numero aleatório entre 1 e 25
-}
+  return Math.floor(Math.random() * 25) + 1;
+};
 
-frm.addEventListener("submit", (e) => {
-    e.preventDefault()                      //submit para gerar nota aleatória
- tocarNota(RandomNumber())
-    
-})
+teclas.forEach((tecla) => {
+  tecla.addEventListener("mousedown", () => clicoumousedown(tecla));
+  tecla.addEventListener("mouseup", () => clicoumouseup(tecla));
+});
 
-
-
-
-
+iniciar.addEventListener("click", () => { 
+  NotaAleatoria = teclas[RandomNumber() - 1].getAttribute('data-nota');
+  tocarNota(NotaAleatoria);
+  resp.innerText = "Acertos: " + contador;
+});
